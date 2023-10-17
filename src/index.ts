b7524e1c10574bb5553f6ae1d6fe84b617fede37
skyclouds2001 <skyclouds2001@163.com>
2023-10-17 13:55:50 +0800

    feat: init com

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 export { default as AudioPlayer } from './audio-player.vue'
+
+export { default as FsTreeSelect } from './fs-tree-select.vue'
+
+export { default as LocalFontSelect } from './local-font-select.vue'
+
+export { default as WritingBoard } from './writing-board.vue'
---
+++ src/writing-board.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'WritingBoard',
+  inheritAttrs: false,
+})
+
+defineExpose({})
+</script>
+
+<template>
+  <div>writing-board</div>
+</template>
+
+<style scoped lang="scss"></style>
