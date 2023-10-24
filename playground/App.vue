3472a793a54f8b74cb882dad88f3c8f773c3fb45
skyclouds2001 <skyclouds2001@163.com>
2023-10-24 23:24:25 +0800

    feat: update playground

--- playground/App.vue
+++ playground/App.vue
@@ -2,29 +2,27 @@
 import { ref } from 'vue'
 import { ControllableComponent, WritingBoard, LocalFontSelect, FsTreeSelect, AudioPlayer } from '@'
 
-type Mode = 'writing-board' | 'controllable-component' | 'local-font-select' | 'fs-tree-select' | 'audio-player'
+const font = ref<any>(null)
 
-const mode = ref<Mode>('audio-player')
-
-const data = ref<any>(null)
+const file = ref<any>(null)
 </script>
 
 <template>
-  <div v-if="mode === 'writing-board'" style="margin: 25px">
+  <div style="margin: 25px">
     <writing-board />
   </div>
-  <div v-if="mode === 'controllable-component'" style="margin: 25px">
+  <div style="margin: 25px">
     <controllable-component />
   </div>
-  <div v-if="mode === 'local-font-select'" style="margin: 25px">
-    <local-font-select v-model="data" />
-    <div>{{ data }}</div>
+  <div style="margin: 25px">
+    <local-font-select v-model="font" />
+    <div>{{ font }}</div>
   </div>
-  <div v-if="mode === 'fs-tree-select'" style="margin: 25px">
-    <fs-tree-select v-model="data" />
-    <div>{{ data }}</div>
+  <div style="margin: 25px">
+    <fs-tree-select v-model="file" />
+    <div>{{ file }}</div>
   </div>
-  <div v-if="mode === 'audio-player'" style="margin: 25px">
+  <div style="margin: 25px">
     <audio-player source="/ls.m4a" />
   </div>
 </template>
