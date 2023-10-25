f13a16ee0572211173136ce7eead7c7f0eccab92
skyclouds2001 <skyclouds2001@163.com>
2023-10-25 12:27:34 +0800

    feat: add some props

--- src/local-font-select.tsx
+++ src/local-font-select.tsx
@@ -1,29 +1,59 @@
 /* global FontData */
 import { ElOption, ElSelect } from 'element-plus'
-import { computed, defineComponent, ref } from 'vue'
+import { type PropType, computed, defineComponent, ref } from 'vue'
 import 'element-plus/es/components/select/style/css'
 import 'element-plus/es/components/option/style/css'
+
+type SelectOptions = Parameters<Exclude<(typeof ElSelect)['setup'], undefined>>[0]
 
 const LocalFontSelect = /* #__PURE__ */ defineComponent({
   name: 'LocalFontSelect',
   inheritAttrs: false,
   props: {
+    postscriptNames: {
+      type: Array as PropType<string[]>,
+      default: null,
+    },
     modelValue: {
       // @ts-expect-error - Local Font Access API
       type: FontData,
       required: true,
     },
+    disabled: {
+      type: Boolean as PropType<Exclude<SelectOptions['disabled'], undefined>>,
+      default: false,
+    },
+    size: {
+      type: String as PropType<Exclude<SelectOptions['size'], undefined>>,
+      default: '',
+    },
     name: {
-      type: String,
+      type: String as PropType<Exclude<SelectOptions['name'], undefined>>,
       default: 'Local Font Select',
     },
     placeholder: {
-      type: String,
+      type: String as PropType<Exclude<SelectOptions['placeholder'], undefined>>,
       default: 'Please select the font',
     },
-    postscriptNames: {
-      type: Boolean,
-      default: null,
+    multiple: {
+      type: Boolean as PropType<Exclude<SelectOptions['multiple'], undefined>>,
+      default: false,
+    },
+    multipleLimit: {
+      type: Number as PropType<Exclude<SelectOptions['multipleLimit'], undefined>>,
+      default: 0,
+    },
+    effect: {
+      type: String as PropType<Exclude<SelectOptions['effect'], undefined>>,
+      default: 'light',
+    },
+    autocomplete: {
+      type: String as PropType<Exclude<SelectOptions['autocomplete'], undefined>>,
+      default: 'off',
+    },
+    filterable: {
+      type: Boolean as PropType<Exclude<SelectOptions['multiple'], undefined>>,
+      default: true,
     },
   },
   emits: {
@@ -43,7 +73,6 @@ const LocalFontSelect = /* #__PURE__ */ defineComponent({
     })
 
     // @ts-expect-error - Local Font Access API
-    // eslint-disable-next-line no-undef
     const fonts = ref<FontData[]>([])
 
     const loadFont = async (): Promise<void> => {
@@ -73,7 +102,7 @@ const LocalFontSelect = /* #__PURE__ */ defineComponent({
 
     return () => (
       <>
-        <ElSelect modelValue={font} onUpdate:modelValue={(value) => (font.value = value)} disabled={!isSupported} name={props.name} placeholder={props.placeholder} ariaLabel={props.name} onVisible-change={loadFont}>
+        <ElSelect modelValue={font} onUpdate:modelValue={(value) => (font.value = value)} disabled={!isSupported && props.disabled} size={props.size} name={props.name} placeholder={props.placeholder} multiple={props.multiple} multipleLimit={props.multipleLimit} effect={props.effect} autocomplete={props.autocomplete} filterable={props.filterable} ariaLabel={props.name} onVisible-change={loadFont}>
           {fonts.value.map((font) => (
             <ElOption key={font.postscriptName} label={font.fullName} value={font} />
           ))}
