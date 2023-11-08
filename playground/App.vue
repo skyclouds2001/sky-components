0e4786cf7a6391804fe90d77e19f7c9cef817323
skyclouds2001 <skyclouds2001@163.com>
2023-11-08 15:56:11 +0800

    style: rename

--- playground/App.vue
+++ playground/App.vue
@@ -23,7 +23,7 @@ const file = ref<any>(null)
     <div>{{ file }}</div>
   </div>
   <div style="margin: 25px">
-    <audio-player source="/media.m4a" />
+    <audio-player source="/audio.m4a" />
   </div>
 </template>
 
