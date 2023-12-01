b238307d462e79c08696d9e70c3aabdc199d501d
skyclouds2001 <skyclouds2001@163.com>
2023-12-01 14:05:31 +0800

    build: cleanup visualizer plugin

--- rollup.config.js
+++ rollup.config.js
@@ -49,12 +49,6 @@ export default defineConfig({
       minimize: true,
       plugins: [autoprefixer()],
     }),
-    visualizer({
-      filename: 'report.html',
-      title: 'report',
-      open: true,
-      gzipSize: true,
-      brotliSize: true,
-    }),
+    visualizer(),
   ],
 })
